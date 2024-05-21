5ab0f9604c8ba83cb997e72b3c5fc8c7e5665478
dwriz <dwikyrizkya27@gmail.com>
2024-05-21 22:17:35 +0700

    fix register

--- client/pages/Register.jsx
+++ client/pages/Register.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
+import "../pages/Register.css";
 
 function Register() {
   const navigate = useNavigate();
@@ -18,9 +19,9 @@ function Register() {
     event.preventDefault();
 
     try {
-      const { data } = await axios({
+      await axios({
         method: "post",
-        url: "http://localhost:3000/register",
+        url: "https://anon-server.dwriz.com/register",
         data: input,
       });
 
@@ -39,31 +40,36 @@ function Register() {
   }
   return (
     <>
-      return (
-      <>
-        <div className="login-box">
-          <h2>Register</h2>
-          <form action="">
-            <div className="user-box">
-              <input type="text" required="" />
-              <label htmlFor="">Username</label>
-            </div>
-            <div className="user-box">
-              <input type="password" required="" />
-              <label htmlFor="">password</label>
-            </div>
-
-            <a href="">
-              <span />
-              <span />
-              <span />
-              <span />
-              Register
-            </a>
-          </form>
-        </div>
-      </>
-      );
+      <div className="register-box">
+        <form onSubmit={handleForm}>
+          <div className="user-box">
+            <input
+              type="text"
+              id="username"
+              autoComplete="off"
+              name="username"
+              onChange={handleChangeInput}
+            />
+            <label htmlFor="">username</label>
+          </div>
+          <div className="user-box">
+            <input
+              type="password"
+              id="password"
+              name="password"
+              onChange={handleChangeInput}
+            />
+            <label htmlFor="">password</label>
+          </div>
+          <button type="submit" className="register-button">
+            <span />
+            <span />
+            <span />
+            <span />
+            Register
+          </button>
+        </form>
+      </div>
     </>
   );
 }
